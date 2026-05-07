0ade1fe2511ffb5eb714e6898d51fb932fbb9263
totallynotharry <162619834+totallynotharry@users.noreply.github.com>
2026-05-07 17:29:40 +0100

    Rebrand interface copy from VAPOR to THUNDER

--- scripts/opt.js
+++ scripts/opt.js
@@ -4,7 +4,7 @@
  */
 
 const cloakPresets = {
-  vapor: { title: "VAPOR v3", favicon: "/_a/logo.new.png" },
+  thunder: { title: "THUNDER v3", favicon: "/_a/logo.new.png" },
   google: { title: "Google", favicon: "https://www.google.com/favicon.ico" },
   gmail: {
     title: "Gmail",
